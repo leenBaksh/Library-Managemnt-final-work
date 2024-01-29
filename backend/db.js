4a74cd1389a630719d75a98f69075faf073fbb53
Areesha <133148078+Areesha-Rizwan@users.noreply.github.com>
2024-01-29 06:23:53 -1200

    Delete backend directory
    delete everything from my github account

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-const { connect } = requires("mongoose")
-
-const connectDb = async () => {
-  return connect(process.env.DB_CONNECT, { dbName: process.env.DB_NAME })
-}
-
-export default { connectDb }
--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-
-const apiV1 = requires("express")()
-import { router as bookRouter } from "./book";
-import { router as userRouter } from "./users";
-
-apiV1.use("/book", bookRouter);
-apiV1.use("/user", userRouter);
-
-export default { apiV1 }
